952259703ab74de450ab73be19369c4cc3c01c74
huynh0201 <phanhuynhcse0210@gmail.com>
2025-04-06 14:48:09 +0700

    connect dbs

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
+import checkconnect from "./config/connectDB";
+import { CHAR } from "sequelize";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 //config view engine
 configViewEngine(app);
+
+//checkconnect
+checkconnect();
 
 //init web routes
 initWebRoutes(app);
